df0320610b1378bc52e886ec444615dd6e0b2288
Joshua Lovelace <rakmial@gmail.com>
2020-08-26 14:18:24 -0700

    set the stage

---
+++ spec/WriteNumber_spec.rb
@@ -0,0 +1,16 @@
+require('rspec')
+require('WriteNumber')
+
+
+expect(WriteNumber) do
+  
+end
+
+### English Specification
+### Requirements: Write a method that translates numbers in numeric (ie Integer)
+### form into written English words. 
+### From simple to complex (kinda, not really - the lower the number, the more
+### special cases, but we can expand to higher values more easily.)
+
+
+# 
