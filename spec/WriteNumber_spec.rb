e6bf874d8ae50e060b0cd10f91b66f7bbfd5ec32
Joshua Lovelace <rakmial@gmail.com>
2020-08-26 17:41:11 -0700

    test 4 red

--- spec/WriteNumber_spec.rb
+++ spec/WriteNumber_spec.rb
@@ -11,13 +11,20 @@ describe(WriteNumber) do
       expect(WriteNumber.new(0).written).to(eq("zero"))
   end
   it('contains Hash instance var @uniqs that takes Integers[0-12] as keys \
-    and their written values as values. .uniqs reader method provided.') do
+    and their written Strings as values. .uniqs reader method provided.') do
       expect(WriteNumber.new(0).uniqs).to(eq({
         0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
         6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten",
         11 => "eleven", 12 => "twelve"
       }))
   end
+  it('contains Hash instance var @teens that takes Integers[13-19] as keys \
+    and their written Strings as values. .teens reader method provided.') do
+      expect(WriteNumber.new(0).teens).to(eq({
+        13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen",
+        17 => "seventeen", 18 => "eighteen", 19 => "nineteen"
+      }))
+    end
 
 end
 
